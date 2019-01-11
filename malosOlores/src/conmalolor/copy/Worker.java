3367d46972384820fe9f482001f63f3670bd71f1
YATIENCIA <33614080+YATIENCIA@users.noreply.github.com>
2019-01-10 22:32:35 -0500

    Corrección de la clase Employee - switch statements/primitive obssesion

---
+++ malosOlores/src/conmalolor/copy/Worker.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conmalolor.copy;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ *
+ * @author IYAC
+ */
+public class Worker extends Employee{
+    
+    public Worker(float salary, float bonusPercentage) {
+        super(salary, bonusPercentage);
+    }
+    
+    @Override
+    public float cs()
+    {
+        Date date = new Date();
+        //Obtiene la hora local
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        //Obtiene el mes en forma de entero
+        int month = localDate.getMonthValue();
+        return calcularDecimo(0, month);
+    }
+    
+    
+    
+    @Override 
+    public float CalculateYearBonus() 
+    {
+        return 0;
+    }
+}
